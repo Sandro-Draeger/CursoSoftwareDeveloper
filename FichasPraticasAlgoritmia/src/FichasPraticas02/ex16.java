ae9e63f20bd7924569f653cd808285a10349a1a5
Sandro-Draeger <sandrodraegerpt@gmail.com>
2025-10-13 12:51:52 +0100

    exnovos

--- FichasPraticasAlgoritmia/src/FichasPraticas02/ex16.java
+++ FichasPraticasAlgoritmia/src/FichasPraticas02/ex16.java
@@ -10,21 +10,18 @@ public class ex16 {
             System.out.print("Insira um valor em euros: ");
             int num = input.nextInt();
 
-            // Notas disponíveis
             int[] euros = {200, 100, 50, 20, 10, 5};
 
-            // Verifica se o valor é múltiplo de 5
             if (num % 5 != 0) {
                 System.out.println("Valor inválido! Deve ser múltiplo de 5.");
-                return; // Sai do programa
+                return;
             }
 
-            // Calcula o troco com o menor número de notas
             for (int nota : euros) {
-                int qtdNotas = num / nota; // Quantas notas desse valor cabem
+                int qtdNotas = num / nota;
                 if (qtdNotas > 0) {
                     System.out.println(nota + "€: " + qtdNotas + " nota(s)");
-                    num %= nota; // Atualiza o valor restante
+                    num %= nota;
                 }
             }
         }
