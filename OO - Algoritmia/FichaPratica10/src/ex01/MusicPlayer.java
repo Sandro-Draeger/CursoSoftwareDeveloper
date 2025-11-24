3c45f72e011edf2ec4c4eac44dc0b6e445907072
Sandro-Draeger <sandrodraegerpt@gmail.com>
2025-11-24 09:20:50 +0000

    24/11

--- OO - Algoritmia/FichaPratica10/src/ex01/MusicPlayer.java
+++ OO - Algoritmia/FichaPratica10/src/ex01/MusicPlayer.java
@@ -11,24 +11,35 @@ public class MusicPlayer {
         this.programacaoMusical = programacaoMusical;
     }
 
-    public void adicionarMusica(Musica addMusica){
-        for (int item = 0; item < programacaoMusical.size(); item++) {
-            if (!programacaoMusical.equals(addMusica)) {
-                programacaoMusical.add();
-                System.out.println("Adicionado com sucesso!");
-            } else {
-                System.out.println("Esta música já esta na playlist.");
-            }
+    public void adicionarMusica(String musica) {
+        if (!programacaoMusical.contains(musica)) {
+            programacaoMusical.add(musica);
+            System.out.println("Adicionada com sucesso");
+        } else {
+            System.out.println("Esta música já está na playlist");
         }
     }
-    public void removerMusica(int indice){
-        for (int item = 0; item < programacaoMusical.size(); item++) {
-              if() {
-                  programacaoMusical.remove(indice);
-              } else {
-                System.out.println("Removido com sucesso!");
-            }
+
+    public void removerMusica(String musica) {
+        if (!programacaoMusical.contains(musica)) {
+            programacaoMusical.remove(musica);
+            System.out.println("Removida com sucesso");
+        } else {
+            System.out.println("Esta não está na sua playlist");
         }
     }
+
+    public void trocarMusicas(String musicaNova, String musicaAntiga){
+        if(!programacaoMusical.contains(musicaAntiga)){
+            musicaAntiga = musicaNova;
+            System.out.println("Troca realizada com sucesso");
+        } else {
+            System.out.println("Musica não encontrada");
+        }
     }
+
+    public void limparProgramacao(){
+        programacaoMusical.removeAll();//o que eu ponho como parametro.
+    }
+}
 
