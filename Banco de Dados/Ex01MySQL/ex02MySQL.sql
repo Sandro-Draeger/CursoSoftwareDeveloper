2660bc1a25d62215bda2c11091c481ae14030e4d
Sandro-Draeger <sandrodraegerpt@gmail.com>
2026-01-06 09:09:23 +0000

    Add stored procedures for client and loan management

--- Banco de Dados/Ex01MySQL/ex02MySQL.sql
+++ Banco de Dados/Ex01MySQL/ex02MySQL.sql
@@ -101,6 +101,73 @@ JOIN cliente c ON e.ClienteID = c.ID
 WHERE e.Data_Devolucao OR e.Data_Devolucao >= '2021-01-01';
 */
 
+/* 5.A 
+DELIMITER $$
+CREATE PROCEDURE sp_GetClientesPorEmailDominio(
+    IN p_dominio VARCHAR(50)
+)
+BEGIN
+    SELECT *sp_GetClientesPorEmailDominio
+    FROM cliente
+    WHERE Email LIKE CONCAT('%', p_dominio, '%');
+END $$
+DELIMITER ;
+
+CALL sp_GetClientesPorEmailDominio('hotmail.com');
+*/
+
+/*5.B
+DELIMITER $$
+CREATE PROCEDURE sp_GetEmprestimosAtivosPorCliente(
+    IN p_ClienteID INT
+)
+BEGIN
+    SELECT *
+    FROM Emprestimo
+    WHERE ClienteID = p_ClienteID
+      AND Status = 'Ativo';
+END $$
+DELIMITER ;
+*/
+
+/*5.C
+DELIMITER $$
+CREATE PROCEDURE sp_GetLivrosMaisEmprestadosPorGenero()
+BEGIN
+    SELECT 
+        l.Genero,
+        l.Titulo,
+        COUNT(e.LivroID) AS TotalEmprestimos
+    FROM Livros l
+    JOIN Emprestimos e ON l.LivroID = e.LivroID
+    GROUP BY l.Genero, l.Titulo
+    ORDER BY l.Genero, TotalEmprestimos DESC;
+END $$
+DELIMITER ;
+*/
+
+/*5.D
+DELIMITER $$
+CREATE PROCEDURE registrarEmprestimo(
+    IN p_LivroID INT,
+    IN p_ClienteID INT,
+    IN p_DataEmprestimo DATE
+)
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM emprestimo
+        WHERE LivroID = p_LivroID
+          AND Data_Devolucao IS NULL
+    ) THEN
+        INSERT INTO emprestimo (LivroID, ClienteID, Data_Emprestimo, Data_Devolucao)
+        VALUES (p_LivroID, p_ClienteID, p_DataEmprestimo, NULL);
+    END IF;
+END$$
+DELIMITER ;
+*/
+
+
 
 
 
